537b51148a517477d6d1c3b9481e34b139bf1efe
jdimawea <94670505+jdimawea@users.noreply.github.com>
2024-04-20 18:07:26 -0600

    index.php completed

--- ProjectDimaweaV4/php/index.php
+++ ProjectDimaweaV4/php/index.php
@@ -1,15 +1,71 @@
 <html>
 <head>
 	<title>Home</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="../style/style.css">
+	<?php include '../inc/head.inc' ; ?>
 </head>
 <body>
 	<div class="grid">
-		<div class="u-header"></div>
-		<div class="i-body"></div>
-		<div class="u-footer"></div>
+	
+		<div class="u-header">
+			<a href='http://localhost/infs3800/ProjectDimaweaV4/php/index.php'><img src='../img/logo.png'></a>
+			<nav>
+				<a href='http://localhost/infs3800/ProjectDimaweaV4/php/index.php'>Home</a>
+				<a href='http://localhost/infs3800/ProjectDimaweaV4/php/about.php'>About Us</a>
+				<a href='http://localhost/infs3800/ProjectDimaweaV4/php/contact.php'>Contact Us</a>
+				<!-- add logoff in php -->
+				<a href='http://localhost/infs3800/ProjectDimaweaV4/php/login.php'>Login</a>
+			</nav>			
+		</div>
+		
+		<div class="u-body i-body">
+			<main>
+				<div class='container1'>
+					<p class='p1'>
+						<span>Technology made Beautiful.</span> <br>
+						<span>A future-focused digital studio</span> <br>
+						<span>passionate about bringing clarity to</span> <br>
+						<span>the complex.</span> <br>
+					</p>
+				</div>
+				<div class='container2'>
+					<img class='img1' src="../img/AiDragon.png">
+					<p>Ai Generated Dragon</p>
+				</div>
+				<div class='container3'>
+					<div class='card1'>
+						<img class='img1' src="../img/f1img.png">
+						<p>Redbull F1</p>
+					</div>
+					<div class='card2'>
+						<img class='img1' src="../img/stardestroyer.png">
+						<p>Imperial Stardestroyer</p>
+					</div>
+				</div>
+			</main>				
+		</div>
+		
+		<div class="u-footer">
+			<div class="left">
+				<p> Sign up for our newsletter<br>
+				<input type="text" name="email" value="-Your email">
+				</p>
+			</div>
+			<div class="right">
+				<p>
+					New Business <br>
+					<a href='mailto:jdimawea@uccs.edu'>jdimawea@uccs.edu</a>
+				</p>
+				<p>
+					About Us <br>
+					<a href='http://localhost/infs3800/ProjectDimaweaV4/php/about.php'>View our story</a>
+				</p>			
+				<p>
+					Location <br>
+					<span class='location'>1420 Austin Bluffs Pkwy<br> Colorado Springs</span>
+				</p>	
+			</div>				
+		</div>
+		
 	</div>
 </body>
 </html>
